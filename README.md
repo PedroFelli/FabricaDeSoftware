9990b9b2bea553505062fd762890fc0761a272df
Lucas Hananni <lucas.hananni@onblox.com.br>
2017-09-05 20:08:33 -0300

    Retirando o projeto antigo

--- README.md
+++
@@ -1,1 +0,0 @@
-Courrier - Fabrica de Software -  UniEvangelica
--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=courrier',
-    'username' => 'root',
-    'password' => '123456',
-    'charset' => 'utf8',
-];
